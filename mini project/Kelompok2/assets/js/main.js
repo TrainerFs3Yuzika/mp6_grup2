aead37a3c08de27d010addf3e5368d4f0919316f
AzisFajar <azismustache@gmail.com>
2024-04-05 08:07:29 +0700

    first commit

---
+++ mini project/Kelompok2/assets/js/main.js
@@ -0,0 +1,68 @@
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			noOpenerFade: true,
+			speed: 300
+		});
+
+	// Nav.
+
+		// Toggle.
+			$(
+				'<div id="navToggle">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
+				
+//update
+document.getElementById('moreInfoButton').addEventListener('click', function(event) {
+	event.preventDefault();
+    Swal.fire({
+        title: 'INFORMASI TAMBAHAN',
+        html: 'Untuk produk produk lebih lengkap. Bisa akses <a href="Product.html" style="color: #007bff; text-decoration: none; font-weight: bold;">PAGE PRODUCT</a>.',
+        icon: 'info'
+    });
+});
+
+})(jQuery);
